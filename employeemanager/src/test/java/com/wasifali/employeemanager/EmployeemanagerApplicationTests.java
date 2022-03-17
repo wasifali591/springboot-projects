c79584222fe7a6d13c5f9c573afbde2bbc169b71
wasifali591 <wasifali591@gmail.com>
2022-03-17 11:02:43 +0530

    new employee manager backend using spring boot

---
+++ employeemanager/src/test/java/com/wasifali/employeemanager/EmployeemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasifali.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
